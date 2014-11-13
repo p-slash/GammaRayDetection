679b5c9a0c32f692aa0b01614f9e610efb8ab914
p-slash <goksel.karacayli@boun.edu.tr>
2014-11-13 16:03:47 +0200

    A pixel cutoff value introduced. New histograms

--- SomeNoiseData/GRanalyse.C
+++ SomeNoiseData/GRanalyse.C
@@ -10,9 +10,9 @@ int PIC_NUMBER    = 0;
 int MAX_PIX_VALUE = 0;
 int DataLength    = 0;
 
-void histogramFill (int *Data, int Len) {
+void histogramFillAll (int *Data) {
     int k, i;
-    int entries = 0;
+    //int entries = 0;
     const char *colors[] = {"Red", "Green", "Blue", "All"};
     char buffer[30];
     TH1I* hists[4];
@@ -26,29 +26,55 @@ void histogramFill (int *Data, int Len) {
 	    hists[k] = new TH1I(colors[k], buffer, MAX_PIX_VALUE + 1, 0, MAX_PIX_VALUE + 1);    
     }
 
-    for (k = 1; k < Len; k++) {    
+    for (k = 1; k < DataLength; k++) {    
         i = k / (MAX_PIX_VALUE + 1);
-        entries += Data[k];
+        //entries += Data[k];
         hists[i]->AddBinContent( k - (i * (MAX_PIX_VALUE + 1)), Data[k]);
         hists[3]->AddBinContent( k - (i * (MAX_PIX_VALUE + 1)), Data[k]);
     }
 
-    hists[3]->setEntries(entries);
-    hists[3]->Draw();
-    sprintf(buffer, "%s.pdf", colors[k]);  
-    gPad->SaveAs(buffer);
-    
-    entries /= 3;
-
-    for (k = 0; k < 3; k++) {
-        hists[k]->setEntries(entries);
+    for (k = 0; k < 4; k++) {
+        //hists[k]->setEntries(entries);
         hists[k]->Draw();
         sprintf(buffer, "%s.pdf", colors[k]);  
         gPad->SaveAs(buffer);
+
+        hists[k]->~TH1I();
     }
+}
+
+void histogramFillHigh(int *Data, int cutoff) {
+    int k, i;
+    const char* colors[] = {"Red", "Green", "Blue", "All"};
+    char buffer[60];
+    TH1I* hists2[4];
+
+    // Start by setting ROOT histogram drawing options
+    gStyle->SetOptFit(1111); // show fit parameters when drawing histograms
     
+    // Initialize the histograms
+    for (k = 0; k < 4; k++) {
+        sprintf(buffer, "Noise of %s Pixels Start Value: %d", colors[k], cutoff);
+        hists2[k] = new TH1I(colors[k], buffer, MAX_PIX_VALUE - cutoff + 1, cutoff, MAX_PIX_VALUE + 1);    
+    }
 
+    for (k = cutoff; k < DataLength; k++) {    
+        i = k / (MAX_PIX_VALUE + 1);
+        
+        if (((k - cutoff) - (i * (MAX_PIX_VALUE + 1))) < 0)
+            k += cutoff;
 
+        hists2[i]->AddBinContent((k - cutoff) - (i * (MAX_PIX_VALUE + 1)), Data[k]);
+        hists2[3]->AddBinContent((k - cutoff) - (i * (MAX_PIX_VALUE + 1)), Data[k]);
+    }
+
+    for (k = 0; k < 4; k++) {
+        hists2[k]->Draw();
+        sprintf(buffer, "%s_%d.pdf", colors[k], cutoff);  
+        gPad->SaveAs(buffer);
+
+        hists2[k]->~TH1I();
+    }
 }
 
 #if !defined(__CINT__) || defined(__MAKECINT__)
@@ -82,8 +108,9 @@ int main() {
 
     fclose(toRead);
     
-    histogramFill(NPixData, DataLength);
-    
+    histogramFillAll(NPixData);
+    histogramFillHigh(NPixData, 20);
+
     delete[] NPixData;
 
     NPixData = NULL;
